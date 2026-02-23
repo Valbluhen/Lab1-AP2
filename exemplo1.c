e4520192e42c29dfc662a2ab2b85c46d456128e4
AnthonySS <111892364+Valbluhen@users.noreply.github.com>
2026-02-23 14:41:07 -0300

    Update exemplo1.c

--- exemplo1.c
+++ exemplo1.c
@@ -2,8 +2,8 @@
 #include <locale.h>
 int main(){
 	setlocale(LC_ALL, "Portuguese");
-  // mensagens
 	printf("Mackenzie - FCI\n");
+	printf("FCI - Fundação 1970\n");
 	printf("Ciencia da Computação\n");
 	printf("Turma - 02N\n");
 	printf("Algoritmos e Programação II\n");
