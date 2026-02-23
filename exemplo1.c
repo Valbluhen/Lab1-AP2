c90bd0124aeb9ff40c967545ac480224f5c778c6
Valbluhen <111892364+Valbluhen@users.noreply.github.com>
2026-02-23 12:33:58 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <locale.h>
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+  // mensagens
+	printf("Mackenzie - FCI\n");
+	printf("Ciencia da Computação\n");
+	printf("Turma - 02N\n");
+	printf("Algoritmos e Programação II\n");
+	return 0;
+}
